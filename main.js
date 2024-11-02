34f1cbfe103e88cd3daae671bfcde483f9ebaf3f
Bagustar <batan.bagustar@gmail.com>
2024-11-02 08:23:51 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+/*
+let score = 69;
+
+if (score >= 90) {
+    console.log ("Отлично");
+} else if (score >= 70) {
+    console.log ("Хорошо");
+} else if (score >= 50) {
+    console.log ("удовлетворительно");
+} else if (score < 50 ) {
+    console.log ("неудовлетворительно");
+} 
+*/
+
+/*let number = 50;
+
+let isEven = (number %2 !== 0) ? `число нечетныое` : `число четное`
+
+console.log (isEven);*/
+
+/*
+let userInput = +prompt("Введите число");
+
+switch (userInput) {
+    case 1:
+        alert("Один");
+        break;
+    case 2:
+        alert("Два");
+        break;
+    case 3:
+        alert("Три");
+        break;
+    case 4:
+        alert("Четыре");
+        break;
+    case 5:
+        alert("Пять");
+        break;
+    default:
+        alert("Неизвестное число");
+}
+*/
+
+/*let a = 50;
+let b = a ?? 100;
+
+console.log(b);*/
+
+/*let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+
+//console.log (numbers [2]);
+
+numbers[4] = 9;
+console.log (numbers);
+
+numbers [10] = 66;
+console.log (numbers.length);
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+console.log (matrix [1][2]);
+
+let fruits = ["Яблоко", "Банан", "Киви"];
+
+fruits.unshift('Груша');
+fruits.push('Хурма'); 
+console.log (fruits);
+
+let lastFruit = fruits[fruits.length - 1];
+
+console.log(lastFruit);
+*/
+
